e50e26b1e7af26cde48d9e21db0aaeb3a7353cd1
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-03-24 19:29:07 -0500

    ✏️Changed LuaLine Foreground

--- lua/lualine/themes/iceberg.lua
+++ lua/lualine/themes/iceberg.lua
@@ -2,36 +2,36 @@ return {
     normal = {
         a = { bg = "#84a0c6", fg = "#17171b" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" }
+        c = { bg = "#0f1117", fg = "#EDEFF0" }
     },
     insert = {
         a = { bg = "#78b892", fg = "#161821" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" }
+        c = { bg = "#0f1117", fg = "#EDEFF0" }
     },
     visual = {
         a = { bg = "#bc83e3", fg = "#161821" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" }
+        c = { bg = "#0f1117", fg = "#EDEFF0" }
     },
     replace = {
         a = { bg = "#e2a478", fg = "#161821" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" }
+        c = { bg = "#0f1117", fg = "#EDEFF0" }
     },
     command = {
         a = { bg = "#a093c7", fg = "#161821" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" }
+        c = { bg = "#0f1117", fg = "#EDEFF0" }
     },
     terminal = {
         a = { bg = "#89b8c2", fg = "#161821" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" }
+        c = { bg = "#0f1117", fg = "#EDEFF0" }
     },
     inactive = {
         a = { bg = "#0F1117", fg = "#17171b" },
         b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#3e445e" },
+        c = { bg = "#0f1117", fg = "#EDEFF0" },
     }
 }
