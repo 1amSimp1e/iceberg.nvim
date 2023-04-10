6881ee52c12a8f5260e919bbbb08bcd1285b1c1c
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-04-10 17:21:27 -0500

    Formated lualine themes

--- lua/lualine/themes/iceberg.lua
+++ lua/lualine/themes/iceberg.lua
@@ -1,37 +1,37 @@
 return {
-    normal = {
-        a = { bg = "#8AB4F8", fg = "#17171b" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" }
-    },
-    insert = {
-        a = { bg = "#78b892", fg = "#161821" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" }
-    },
-    visual = {
-        a = { bg = "#bc83e3", fg = "#161821" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" }
-    },
-    replace = {
-        a = { bg = "#e2a478", fg = "#161821" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" }
-    },
-    command = {
-        a = { bg = "#a093c7", fg = "#161821" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" }
-    },
-    terminal = {
-        a = { bg = "#89b8c2", fg = "#161821" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" }
-    },
-    inactive = {
-        a = { bg = "#0F1117", fg = "#17171b" },
-        b = { bg = "#2e313f", fg = "#6b7089" },
-        c = { bg = "#0f1117", fg = "#EDEFF0" },
-    }
+	normal = {
+		a = { bg = "#7e9cd8", fg = "#17171b" },
+		b = { bg = "#2e313f", fg = "#6b7089" },
+		c = { bg = "#0f1117", fg = "#EDEFF0" },
+	},
+	insert = {
+		a = { bg = "#78b892", fg = "#161821" },
+		b = { bg = "#2e313f", fg = "#6b7089" },
+		c = { bg = "#0f1117", fg = "#EDEFF0" },
+	},
+	visual = {
+		a = { bg = "#bc83e3", fg = "#161821" },
+		b = { bg = "#2e313f", fg = "#6b7089" },
+		c = { fg = "#727169", bg = nil },
+	},
+	replace = {
+		a = { bg = "#e2a478", fg = "#161821" },
+		b = { bg = "#2e313f", fg = "#6b7089" },
+		c = { bg = "#0f1117", fg = "#EDEFF0" },
+	},
+	command = {
+		a = { bg = "#a093c7", fg = "#161821" },
+		b = { bg = "#2e313f", fg = "#6b7089" },
+		c = { bg = "#0f1117", fg = "#EDEFF0" },
+	},
+	terminal = {
+		a = { bg = "#89b8c2", fg = "#161821" },
+		b = { bg = "#2e313f", fg = "#6b7089" },
+		c = { bg = "#0f1117", fg = "#EDEFF0" },
+	},
+	inactive = {
+        a = { fg = "#727169", bg = "#16161D", gui = "bold" },
+        b = { fg = "#727169", bg = "#16161D" },
+		c = { fg = "#727169", bg = nil },
+	},
 }
